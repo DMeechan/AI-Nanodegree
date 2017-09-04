4ee5a9ded1b0d5ca039dafafa6f665c189968f42
DMeechan <daniel.meechan.1+git@gmail.com>
2017-09-04 12:30:34 +0100

    Set up project files

--- Sudoku/My Code/functions.py
+++ Sudoku/My Code/functions.py
@@ -1,5 +1,6 @@
 from utils import *
 
+
 def grid_values(input):
     all_digits = '123456789'
 
@@ -70,11 +71,51 @@ def reduce_puzzle(values):
 
     return values
 
+
+def search(values):
+    # First reduce the puzzle using Elimination and Only Choice:
+    values = reduce_puzzle(values)
+
+    if values is False:
+        return False
+
+     # Solved!
+    if all(len(values[box]) == 1 for box in values.keys()):
+        return values
+
+    # Find an incomplete square with the fewest options
+    incomplete_boxes = [box for box in values.keys() if len(values[box]) > 1]
+
+    chosen_box = ''
+    num_of_options = 9
+
+    for box in incomplete_boxes:
+        if len(values[box]) < num_of_options:
+            num_of_options = len(values[box])
+            chosen_box = box
+        # Stop searching if len = 2 because it can't get any lower
+        if len(values[box]) == 2:
+            break
+
+    # Recursively solve each of the resulting puzzle sand it one returns a
+    # value, we use it!
+    for possible_value in values[chosen_box]:
+        altered_values = values.copy()
+        altered_values[chosen_box] = possible_value
+
+        attempt = search(altered_values)
+
+        if attempt:
+            return attempt
+
+    return False
+
+
 input1 = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 input2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 
 input_dict = grid_values(input2)
 # print("Grid values: ", grid_values(input))
 
-reduce_puzzle_values = reduce_puzzle(input_dict)
-print('Reduced puzzle: ', reduce_puzzle_values)
+search_values = search(input_dict)
+print('Searched puzzle: ', search_values)
