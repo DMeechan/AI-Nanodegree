217c561296dde3d58d17092d3fe8015a6130ce1b
Daniel Meechan <daniel.meechan@ibm.com>
2018-03-15 02:36:56 +0000

    Completed parts 1 and 2 of dog project

--- experimentation/convolutional-neural-networks/conv-dims.py
+++ experimentation/convolutional-neural-networks/conv-dims.py
@@ -2,13 +2,29 @@ from keras.models import Sequential
 from keras.layers import Conv2D
 
 model = Sequential()
+# conv_layer = Conv2D(
+#     filters=16, 
+#     kernel_size=2, 
+#     strides=2, 
+#     padding='valid', 
+#     activation='relu',
+#     input_shape=(200, 200, 1)
+# )
 conv_layer = Conv2D(
     filters=32, 
-    kernel_size=4, 
+    kernel_size=3, 
     strides=2, 
-    padding='valid', 
+    padding='same', 
     activation='relu',
-    input_shape=(500, 500, 1)
+    input_shape=(128, 128, 3)
 )
+# conv_layer = Conv2D(
+#     filters=32, 
+#     kernel_size=4, 
+#     strides=2, 
+#     padding='valid', 
+#     activation='relu',
+#     input_shape=(500, 500, 1)
+# )
 model.add(conv_layer)
 model.summary()
